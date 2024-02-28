8c19aa2444be84028719aee558824b17702e764c
Yalcin <bilgekagan902@gmail.com>
2024-02-28 15:23:08 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace operatorler{
+    class Program{
+        public static void Main(string[] args){
+            // Atama Ve işlemli Atama
+            int x = 3;
+            int y = 3;
+            y = y+2;
+            y +=2;
+            Console.WriteLine(y);
+            y += 2;
+            Console.WriteLine(y);
+            y /= 1;
+            Console.WriteLine(y);
+            x*=2;
+            Console.WriteLine(x);
+            bool isSuccess =true;
+            bool isCompleted = false;
+            if(isSuccess && isCompleted)
+                Console.WriteLine("Perfect!");
+            if(isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+            if(isSuccess && !isCompleted) 
+                Console.WriteLine("Fine");
+                // ilişkisel Operatoler
+
+                int a = 3;
+                int b = 4;
+                bool sonuc = a<b;
+                Console.WriteLine("sonuc");
+                 sonuc = a>b;
+                Console.WriteLine("sonuc");
+                 sonuc = a>=b;
+                Console.WriteLine("sonuc");
+                 sonuc = a<=b;
+                Console.WriteLine("sonuc");
+                 sonuc = a==b;
+                Console.WriteLine("sonuc");
+                 sonuc = a!=b;
+                Console.WriteLine("sonuc");
+
+                int sayi1 = 10;
+                int sayi2 = 5;
+                int sonuc1 = sayi1/sayi2;
+                Console.WriteLine(sonuc1);
+                sonuc1 = sayi1*sayi2;
+                Console.WriteLine(sonuc1);
+                sonuc1 = sayi1+sayi2;
+                Console.WriteLine(sonuc1);
+                sonuc1 = sayi1++;
+                Console.WriteLine(sonuc1);
+
+                int sonuc2 = 20%3;
+                Console.WriteLine(sonuc2);
+
+        }
+    }
+
+}
